0c8e1c4223513ba92d7a435059cd904c1a28fd63
Dani <daniel.cano@doga.es>
2018-05-25 16:55:44 +0200

    changes in delayed time (fps)

--- pyQt_Test.py
+++ pyQt_Test.py
@@ -7,9 +7,9 @@ from posCurrTorqArduComm import posCurrTorqArduComm
 
 '''CONSTANTS'''
 # (tiempo, par, tension)
-CICLO = ((5, 255, 24),
-         (5, 123, 24),
-         (5, 0, 24))
+CICLO = ((2*5, 255, 24),
+         (3, 123, 24),
+         (3, 0, 24))
 SAMPLING = 100.0  # ms
 ARD_COM = 'COM4'
 
@@ -39,7 +39,6 @@ class App(QtGui.QMainWindow):
         self.x = np.linspace(0, 50., num=100)
         self.X, self.Y = np.meshgrid(self.x, self.x)
         self.counter = 0
-        self.fps = 0.
         self.lastupdate = time.clock()
         self.xdata = []  # init time x data
         self.ydata = []  # init ydata
@@ -49,40 +48,39 @@ class App(QtGui.QMainWindow):
         self.ardu = posCurrTorqArduComm(port='COM4')
 
         #### Start  #####################
-        self.timeout = time.clock()+10  # seconds
+        self.timeout = time.clock() + CICLO[0][0]  # seconds
         self._update()
 
     def _update(self):
 
-        # self.ydata = np.sin(self.x/3. + self.counter/9.)
-        a = self.ardu.set_torq(self.counter)
+        a = self.ardu.set_torq(CICLO[self.counter][1])  # get data from ardu
+        now = time.clock()
+
         self.ydata = np.append(self.ydata, [a[0]])
         self.y2data = np.append(self.y2data, [a[1]])
-        self.xdata = np.append(self.xdata, time.clock())
+        self.xdata = np.append(self.xdata, now)
 
         self.h2.setData(self.xdata, self.ydata)
         self.h1.setData(self.xdata, self.y2data)
-
-        now = time.clock()
 
         print now, a[0], a[1], self.counter
 
         dt = (now-self.lastupdate)
         if dt <= 0:
             dt = 0.000000000001
-        fps2 = 1.0 / dt
-        self.lastupdate = now
-        self.fps = self.fps * 0.9 + fps2 * 0.1
-#         tx = 'Mean Frame Rate:  {fps:.3f} FPS'.format(fps=self.fps)
+
         tx = 'dt: {dt:.3f}, elapsed: {el:.3f} s'.format(dt=dt, el=now)
         self.label.setText(tx)
-        if now < self.timeout:
-            QtCore.QTimer.singleShot(1, self._update)
 
-        #  counter to increase the torque
-        self.counter += 1
-        if self.counter > 255:
-            self.counter = 0
+        if now > self.timeout:
+            self.counter += 1
+            if self.counter < len(CICLO):
+                self.timeout = time.clock() + CICLO[self.counter][0]
+
+        if self.counter < len(CICLO):
+            QtCore.QTimer.singleShot(SAMPLING, self._update)
+
+        self.lastupdate = now
 
 
 if __name__ == '__main__':
